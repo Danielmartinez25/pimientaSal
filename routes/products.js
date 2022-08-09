c871316122be40ff5dfc9902e64ee6c02f7960a4
Danimartinez25 <danimartinez1325@gmail.com>
2022-08-09 02:43:19 -0300

    Proyecto PG

---
+++ routes/products.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+const {detail} = require('../controllers/productController');
+/* /products */
+router.get('/detail/:id', detail);
+
+module.exports = router;
